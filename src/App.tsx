038380b2baa05ae7accdb184f5f100a3fb10d859
0fprod <franp.dev@protonmail.com>
2024-04-26 09:48:22 +0100

    🎉 initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Balances } from './components/balances/Balances';
+import { Github } from './components/github/Github';
+import { HeroApp } from './components/hero-app/HeroApp';
+import { Collateralized } from './components/collateralized/Collateralized';
+import { Nav } from './components/nav/Nav';
+import { Prices } from './components/prices/Prices';
+import { Stats } from './components/stats/Stats';
+
+function App() {
+  return (
+    <main>
+      <Github />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { rainbowWallet, coinbaseWallet, metaMaskWallet, trustWallet } from '@rainbow-me/rainbowkit/wallets';
+
+const queryClient = new QueryClient();
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [sepolia],
+  ssr: false,
+  wallets: [
+    {
+      groupName: 'Popular',
+      wallets: [metaMaskWallet, rainbowWallet, coinbaseWallet, trustWallet],
+    },
+  ],
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider modalSize="compact">
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);
