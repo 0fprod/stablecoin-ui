ca4de9821e999c8a0275271150498fbe761617cf
0fprod <franp.dev@protonmail.com>
2024-04-27 16:27:30 +0100

    refactor: prices

--- src/components/prices/Prices.tsx
+++ src/components/prices/Prices.tsx
@@ -1,13 +1,15 @@
-import { Loading, PlanCard, Typography } from '@web3uikit/core';
+import { LinkTo, Loading, PlanCard, Typography } from '@web3uikit/core';
 import './Prices.css';
 import { usePrices } from '../../hooks/usePrices';
 import { formatUnits } from 'viem';
-import { useEffect } from 'react';
+import { ReactElement, useEffect } from 'react';
+import { Eth, Chainlink } from '@web3uikit/icons';
+import { Token } from '../../constants/symbols';
 
 export const Prices: React.FC = () => {
   const { linkPrice, ethPrice, updatePrices, isLoading } = usePrices();
   const width = '290px';
-  const height = '300px';
+  const height = '350px';
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -19,55 +21,57 @@ export const Prices: React.FC = () => {
     };
   }, [updatePrices]);
 
+  const renderPlanCard = (title: Token, token: Token, price: bigint, icon: ReactElement, address: string) => {
+    return (
+      <div
+        key={token}
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <PlanCard
+          description={
+            <Typography variant="caption14" weight="550" key={`${token}-feed`}>
+              Via Chainlink Data Feeds
+            </Typography>
+          }
+          features={[]}
+          width={width}
+          height={height}
+          horizontalLine
+          isCurrentPlan
+          ctaButton={
+            <div className="faucet" key={`${token}-cta`}>
+              <LinkTo icon={icon} address={address} text="Faucet" type="external" />
+            </div>
+          }
+          price={<PriceDisplay isLoading={isLoading} value={price} key={`${token}-price`} />}
+          themeColor="rgba(103, 58, 194, 0.6)"
+          title={title}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>Prices</h2>
       <div className="prices">
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <PlanCard
-            description={
-              <Typography color="white" variant="caption14" weight="550" key="link-feed">
-                Via Chainlink Data Feeds
-              </Typography>
-            }
-            features={[]}
-            height={height}
-            width={width}
-            horizontalLine
-            isCurrentPlan
-            price={<PriceDisplay isLoading={isLoading} value={linkPrice} key="link" />}
-            themeColor="rgba(103, 58, 194, 0.6)"
-            title="Link"
-          />
-        </div>
-        <div
-          key="WETH"
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <PlanCard
-            description={
-              <Typography color="white" variant="caption14" weight="550" key="eth-feed">
-                Via Chainlink Data Feeds
-              </Typography>
-            }
-            features={[]}
-            height={height}
-            width={width}
-            horizontalLine
-            isCurrentPlan
-            price={<PriceDisplay isLoading={isLoading} value={ethPrice} key="eth" />}
-            themeColor="rgba(103, 58, 194, 0.6)"
-            title="wETH"
-          />
-        </div>
+        {renderPlanCard(
+          Token.Link,
+          Token.Link,
+          linkPrice,
+          <Chainlink fontSize="2rem" />,
+          'https://faucets.chain.link/sepolia'
+        )}
+        {renderPlanCard(
+          Token.wETH,
+          Token.wETH,
+          ethPrice,
+          <Eth fontSize="2rem" />,
+          'https://sepolia.etherscan.io/address/0xdd13E55209Fd76AfE204dBda4007C227904f0a81#writeContract#F5'
+        )}
       </div>
     </div>
   );
@@ -88,7 +92,7 @@ const PriceDisplay: React.FC<PriceDisplayProps> = ({ isLoading, value }) => {
   }
 
   return (
-    <Typography color="white" variant="h1" weight="700">
+    <Typography variant="h1" weight="700">
       {formatWithDecimals(value)} $
     </Typography>
   );
@@ -99,5 +103,5 @@ const formatWithDecimals = (value: bigint) => {
   const valueLength = value.toString().length;
   return formatUnits(value, 8)
     .toString()
-    .slice(0, valueLength - 3);
+    .slice(0, valueLength - 4);
 };
