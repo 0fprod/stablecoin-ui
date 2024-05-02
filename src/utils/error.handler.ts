398cfa699a0789c06a5e7ca626e527c62306c92f
0fprod <franp.dev@protonmail.com>
2024-05-02 12:45:09 +0100

    refactor: update tests addresses and abis

--- src/utils/error.handler.ts
+++
@@ -1,8 +0,0 @@
-export const getDetails = (errorMessage: string) => {
-  const detailsSection = errorMessage.split('Details: ')[1];
-  if (detailsSection) {
-    return detailsSection.split('\n')[0];
-  } else {
-    return '';
-  }
-};
